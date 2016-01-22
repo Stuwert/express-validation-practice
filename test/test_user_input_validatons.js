7dfdc2e740491595039cb69ab768e046c2c98148
Stuart Urback <stuart@urback.net>
2016-01-22 12:19:27 -0700

    Complete tests

--- test/test_user_input_validatons.js
+++ test/test_user_input_validatons.js
@@ -8,7 +8,7 @@ describe('User Validations', function () {
     it('Name cannot be blank', function (done) {
       validate.nameIsNotBlank.should.be.a('function');
       assert.equal(validate.nameIsNotBlank('   '), 'Name cannot be blank');
-      assert.equal(validate.nameIsNotBlank('George Orwell'), '');
+      assert.equal(validate.nameIsNotBlank('George Orwell'), true);
       done();
     });
   });
@@ -16,7 +16,7 @@ describe('User Validations', function () {
     it('should not be blank', function (done) {
       validate.emailIsValid.should.be.a('function');
       assert.equal(validate.emailIsValid('    '), 'Email is invalid')
-      assert.equal(validate.emailIsValid('me@me.com'), '')
+      assert.equal(validate.emailIsValid('me@me.com'), true)
       done();
     });
   });
