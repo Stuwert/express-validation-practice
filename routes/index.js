131f6d1d2046f41bd98252e35ae94abf55cd08bf
Stuart Urback <stuart@urback.net>
2016-01-22 11:57:16 -0700

    Complete test set 1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {  
+router.get('/', function(req, res, next) {
   res.render('index');
 });
+
+router.post('/', function(req, res, next){
+  if(nameIsNotBlank(req.body.name)){
+    res.redirect('/')
+  }
+})
+
+
 
 module.exports = router;
